c6ab5b9a542f970020352df87d2a8a833ba1b58a
waxir <danishdarman06@gmail.com>
2026-02-28 20:21:51 +0500

    function to remove duplicate from an array

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-let stringstr = 'apple cannot be eat without knife apple is apple and orange is orange'
+let stringstr =
+  "apple cannot be eat without knife apple is apple and orange is orange";
 
-let words = stringstr.split('')
+let words = stringstr.split("");
 
-let countword = {}
+let countword = {};
 
-words.forEach(word => {
-    countword[word] = (countword[word] || 0) + 1
-    console.log(countword)
+words.forEach((word) => {
+  countword[word] = (countword[word] || 0) + 1;
+  console.log(countword)
 });
 
-
-let arr = [1,[2,3],[4,[5,6]]]
+let arr = [1, [2, 3], [4, [5, 6]]];
 
 function flateArray() {
-    return arr.flat(Infinity)
+  return arr.flat(Infinity);
 }
 
-console.log(flateArray())
+console.log(flateArray());
+
+let duplicate = [
+  { id: 1, name: "danish" },
+  { id: 2, name: "aman" },
+  { id: 1, name: "danish" },
+  { id: 2, name: "aman" },
+];
+
+function removeDuble(ar) {
+  const map = new Map();
+
+  ar.forEach((obj) => {
+    map.set(obj.id, obj);
+  });
+
+  return Array.from(map.values());
+}
+
+console.log(removeDuble(duplicate));
