7402a681bb527dfdc99bb0fca41ea2f74cd7afdd
waxir <danishdarman06@gmail.com>
2026-03-14 21:56:22 +0500

    Closure is commited

--- app.js
+++ app.js
@@ -61,3 +61,24 @@ deboncefunction("aman");
 deboncefunction("danish");
 deboncefunction("waxir");
 deboncefunction("rahim");
+
+
+// Closures
+
+function outer(){
+  let count = 0
+
+  return function(){
+    count++
+    console.log(count)
+  }
+}
+
+const counter = outer()
+
+
+counter()
+counter()
+counter()
+counter()
+counter()
